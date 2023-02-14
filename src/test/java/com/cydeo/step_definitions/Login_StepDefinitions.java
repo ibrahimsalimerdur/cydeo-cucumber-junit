fedd47b712eb64a4fbad0316da4aaa91156bca34
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-02-14 10:43:22 +0100

    some progress in example @librarian @admin @student

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,28 +1,43 @@
 package com.cydeo.step_definitions;
 
+
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Login_StepDefinitions {
     @When("user enters librarian username")
     public void user_enters_librarian_username() {
-
+        System.out.println("user enters librarian username");
     }
+
     @When("user enters librarian password")
     public void user_enters_librarian_password() {
-
+        System.out.println("user enters librarian password");
     }
+
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
-
+        System.out.println("user should see the dashboard");
     }
+
     @When("user enters student username")
     public void user_enters_student_username() {
-
+        System.out.println("user enters student username");
     }
+
     @When("user enters student password")
     public void user_enters_student_password() {
-
+        System.out.println("user enters student password");
     }
 
+    @And("user enters admin password")
+    public void userEntersAdminPassword() {
+        System.out.println("user enters admin password");
+    }
+
+    @When("user enters admin username")
+    public void userEntersAdminUsername() {
+        System.out.println("user enters admin username");
+    }
 }
