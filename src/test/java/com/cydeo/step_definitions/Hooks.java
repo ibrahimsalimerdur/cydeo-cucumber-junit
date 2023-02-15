21eea00fbd86013ae74180af4cb98ccd9ce55082
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-02-15 15:19:08 +0100

    Hook class setup with Before method

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,4 +1,18 @@
 package com.cydeo.step_definitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
+
+    @Before
+    public void setupScenario (){
+        System.out.println("====Seting Up browser using cucumber @Before");
+    }
+
+    @After
+    public void tearDownScenario (){
+        System.out.println("====Closing browser using cucumber @Before");
+    }
+
 }
