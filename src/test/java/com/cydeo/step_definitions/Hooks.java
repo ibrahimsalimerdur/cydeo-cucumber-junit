4b93349ec8c93b598443d8faf8e396e129766401
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-02-19 10:30:20 +0100

    Day16_Task

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,11 +1,13 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 public class Hooks {
 
-    @Before
+    @Before ("@student")
     public void setupScenario (){
         System.out.println("====Seting Up browser using cucumber @Before");
     }
@@ -13,6 +15,16 @@ public class Hooks {
     @After
     public void tearDownScenario (){
         System.out.println("====Closing browser using cucumber @Before");
+        System.out.println("Scenario finished, take SS if fails");
     }
 
+    @BeforeStep
+    public void setupStep (){
+        System.out.println("---> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep (){
+        System.out.println("---> applying tearDown using @AfterStep");
+    }
 }
