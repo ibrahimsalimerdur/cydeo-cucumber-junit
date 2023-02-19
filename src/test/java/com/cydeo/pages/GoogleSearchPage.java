97b8fdd494c47a889cc70582501ed73bb1947fdd
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-02-19 13:40:36 +0100

    GoogleSearchPage Locator FindBy() folder Day16_Task

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleSearchPage {
@@ -8,6 +10,15 @@ public class GoogleSearchPage {
     public GoogleSearchPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy (xpath = "//div[.='Accept all']")
+    public WebElement cookieManagement;
+
+    @FindBy (xpath = "//input[@name='q']")
+    public WebElement searchBox;
+
+
+
 
 
 
