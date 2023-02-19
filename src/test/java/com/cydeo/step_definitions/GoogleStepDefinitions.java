beb6a56f9107f4552dd634cbab7b4fa9aca948ea
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-02-19 14:36:46 +0100

    GoogleSearchPage AssertEqual created folder Day16_Task

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,20 +1,27 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
 
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
     @When("user types apple and clicks enter")
     public void user_types_apple_and_clicks_enter() {
-
+        googleSearchPage.cookieManagement.click();
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
 
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
-
+        String expectedTitle = "apple - Google Search";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle);
     }
 
     @When("user is on Google search page")
