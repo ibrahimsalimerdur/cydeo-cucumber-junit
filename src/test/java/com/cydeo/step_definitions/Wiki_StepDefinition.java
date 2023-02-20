56256b9ae063c94316e417560aa182f8a4757c00
Salim Erdur <ibrahimsalimerdur@gmail.com>
2023-02-20 21:09:43 +0100

    Wikipedia search SteveJobs with POM design framework

--- src/test/java/com/cydeo/step_definitions/Wiki_StepDefinition.java
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinition.java
@@ -1,11 +1,16 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.WikiSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class Wiki_StepDefinition {
+
+    WikiSearchPage wikiSearchPage = new WikiSearchPage();
 
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
@@ -14,17 +19,17 @@ public class Wiki_StepDefinition {
 
     @When("User types {string} in the wiki search box")
     public void user_types_in_the_wiki_search_box(String string) {
-
+        wikiSearchPage.wikiSearchPrompt.sendKeys(string);
     }
 
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
-
+        wikiSearchPage.searchButton.click();
     }
 
     @Then("User sees {string} is in the wiki title")
     public void user_sees_is_in_the_wiki_title(String string) {
-
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
     }
 
 
